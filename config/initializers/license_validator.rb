b716e2d3bf9522f26fab38de3acd1593d4a02223
xbwu <xbwu@outlook.com>
2013-07-12 18:40:01 +0800

    license

--- config/initializers/license_validator.rb
+++ config/initializers/license_validator.rb
@@ -6,14 +6,36 @@ license_key_file = '/usr/share/PracticalTraining/License/license_key'
 license_file = '/usr/share/PracticalTraining/License/license'
 passphrase = 'Practical Training 2013'
 
-# encrypted_string = File.read(license_file)
+if (!File.exists?(license_key_file))
+  raise 'license_key file not found in '+license_key_file
+end
+if (!File.exists?(license_file))
+  raise 'license file not found in '+license_file
+end
 
-# private_key = OpenSSL::PKey::RSA.new(File.read(license_key_file), passphrase)
-# decrypted_license = private_key.private_decrypt(Base64.decode64(encrypted_string))
-# licensed_host = decrypted_license.lines.first
-# licensed_ip = decrypted_license
-#
-# local_host = Socket.gethostname
-# local_ips = Socket.ip_address_list
+encrypted_string = File.read(license_file)
 
+private_key = OpenSSL::PKey::RSA.new(File.read(license_key_file), passphrase)
+decrypted_license = private_key.private_decrypt(Base64.decode64(encrypted_string))
+arr = decrypted_license.split("\n")
+licensed_host = arr[0]
+licensed_ip = arr[1]
 
+local_host = Socket.gethostname
+local_ips = Socket.ip_address_list.find_all{|intf| intf.ipv4?}
+
+if (!licensed_host.eql?(local_host))
+  raise 'not licensed to this server'
+end
+
+ip_verified = false
+local_ips.each do |ip|
+  strip = ip.ip_address
+  if (strip.eql?(licensed_ip))
+    ip_verified = true
+  end
+end
+
+if (!ip_verified)
+  raise 'no licensed IP found in this server'
+end
